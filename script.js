997aa52424926dfba49b811f57d9b7afa2d1e1ba
patimontero <pattimontero@hotmail.com>
2018-08-26 16:29:10 -0400

    Added sitemap.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// Menu
 var btn_menu = document.querySelector('.btn-menu');
 var btn_close_menu = document.querySelector('.btn-close-menu');
 var menu = document.querySelector('.sidenav');
-
 btn_menu.addEventListener('click', function () {
     menu.classList.add('is-visible');
 });
 btn_close_menu.addEventListener('click', function () {
     menu.classList.remove('is-visible');
 });
+// Accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -17,10 +18,8 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
     }
 }
-
 // Top
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("top").style.display = "block";
@@ -28,7 +27,6 @@ function scrollFunction() {
         document.getElementById("top").style.display = "none";
     }
 }
-
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera

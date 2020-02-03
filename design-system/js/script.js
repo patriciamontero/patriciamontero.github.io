a3028f91dd21b0fc2750ece45ac05ffde7dd231c
Montero Patricia <Patricia.Montero@loto-quebec.com>
2020-02-03 12:07:49 -0500

    IE fix on script file.

--- design-system/js/script.js
+++ design-system/js/script.js
@@ -3,15 +3,15 @@
 (function() {
   const headings = document.querySelectorAll('h4');
   
-  Array.prototype.forEach.call(headings, h => {
+  Array.prototype.forEach.call(headings, function(h) {
     let btn = h.querySelector('button');
     let target = h.nextElementSibling;
     
-    btn.onclick = () => {
+    btn.onclick = function() {
       let expanded = btn.getAttribute('aria-expanded') === 'true';
       
       btn.setAttribute('aria-expanded', !expanded);
       target.hidden = expanded;  
     }
   });
-})()
+ })()
